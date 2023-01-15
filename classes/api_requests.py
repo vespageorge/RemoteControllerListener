b4e29cf36a465ea1d979e344344ff79fe0409ed3
vespageorge <viespe.georgesorin@gmail.com>
2023-01-15 02:16:01 +0200

    REMOTE-9: Update run_cmd to use all command_types

--- classes/api_requests.py
+++ classes/api_requests.py
@@ -5,7 +5,7 @@ class Requests:
     '''Class used to do all request to API'''
 
     url = None
-    last_resp = None
+    setup_data = None
     header = {
         "Content-type": "application/x-www-form-urlencoded",
         "Accept": "text/plain"
@@ -15,23 +15,23 @@ class Requests:
         ip_address = host_data['DOMAIN']
         port = host_data['PORT']
         self.url = ip_address + ":" + port + "/api"
-        response = requests.get(str(self.url) + "/get-setup/" + str(hwid), timeout=10)
+        response = requests.get(str(self.url) + "/get-setup/" + str(hwid))
         print(response.status_code)
 
     def get_command(self, cmd_id):
         '''Method used to do get command as json format'''
-        response = requests.get(self.url + "/get-commands/" + str(cmd_id), timeout=10)
+        response = requests.get(self.url + "/get-commands/" + str(cmd_id))
         return response.json()
 
     def post_new_setup(self, payload):
         '''Method used to do create new setups'''
         path = str(self.url) + "/post-setup"
-        response = requests.post(path, data=payload, headers=self.header, timeout=10)
-        self.last_resp = response.json()
+        response = requests.post(path, data=payload, headers=self.header)
+        self.setup_data = response.json()
 
     def update_cmd_status(self, cmd_id):
         '''Method used to update status on specific command used'''
         path = str(self.url) + "/update-cmd-status/" + str(cmd_id)
-        response = requests.get(path, timeout=10)
+        response = requests.get(path)
         if response.status_code == 200:
             print("Aplicatie deschisa cu success")
