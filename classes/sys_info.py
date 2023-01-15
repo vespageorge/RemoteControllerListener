e29b3fbef9d632292f76897367c10a121fec9a4e
vespageorge <viespe.georgesorin@gmail.com>
2023-01-15 02:33:36 +0200

    REMOTE-9: Fix bug run_cmd to use all command_types

--- classes/sys_info.py
+++ classes/sys_info.py
@@ -21,7 +21,7 @@ class SystemInfo:
             self.info['platform']=platform.system()
             self.info['hostname']=socket.gethostname()
             self.info['ip_address']=socket.gethostbyname(socket.gethostname())
-            self.info['mac_address']=':'.join(re.findall('..', '%012x' % uuid.getnode()))
+            self.info['mac_address']=':'.join(re.findall('..', '%012x' % uuid.getnode())) # pylint: disable=C0209
             self.info['hwid'] = str(subprocess.check_output('wmic csproduct get uuid'), 'utf-8')
             self.info['hwid'] = self.info['hwid'].split('\n')[1].strip()
             return self.info
@@ -30,13 +30,13 @@ class SystemInfo:
 
     def run_cmd(self,command):
         '''Method used to do get data from current setup'''
-        if command['namme'] == "executable":
-            subprocess.call([command])
-        elif command['namme'] == "cmd":
-            subprocess.call([command])
-        elif command['namme'] == "selenium":
-            print("selenium")
-        elif command['namme'] == "installer":
-            print("installer")
+        if command['name'] == "executable":
+            print(command['name'])
+        elif command['name'] == "cmd":
+            print(command['name'])
+        elif command['name'] == "selenium":
+            print(command['name'])
+        elif command['name'] == "installer":
+            print(command['name'])
         else:
-            print("custom")
+            print(command['name'])
